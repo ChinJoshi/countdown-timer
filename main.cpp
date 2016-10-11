6e05767580dccb74ce06e992b33d6c9ba653845b
ChinJoshi <cshjoshi@gmail.com>
2016-10-11 09:51:11 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,18 +2,22 @@
 #include <thread>
 #include <iostream>
 
-void timer(int timeAmt){
-    int* timeAmtPointer = &timeAmt;
-    int pointerTo = *timeAmtPointer;
-    for (int x = 0;x<=pointerTo;--pointerTo){
-        std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(1));
-        std::cout<<"Time Left : "<<pointerTo<<std::endl;
-    }
-}
+class Timer {
+public:
+	void timer(int timeAmt) {
+		int* timeAmtPointer = &timeAmt;
+		int pointerTo = *timeAmtPointer;
+		for (int x = 0; x <= pointerTo; --pointerTo) {
+			std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(1));
+			std::cout << "Time Left : " << pointerTo << std::endl;
+		}
+	}
+};
 
 int main() {
-    int timeAmt;
-    std::cout<<"Enter how many seconds you would like to count"<<std::endl;
-    std::cin>>timeAmt;
-    timer(timeAmt);
+	Timer* foo = new Timer;
+	int timeAmt;
+	std::cout << "Enter how many seconds you would like to count" << std::endl;
+	std::cin >> timeAmt;
+	foo->timer(timeAmt);
 }
